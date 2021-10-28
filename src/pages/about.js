d556fa466954f5d52f12904ac19d9cdf4b3205e2
Neide Urbina <neide.n.urbina@gmail.com>
2021-10-27 22:33:48 -0500

    Routes created, routes work, stock dashboard displays

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = (props) => {
+    return <h1>Display About data</h1>
+}
+
+export default About
---
+++ src/pages/stock.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Stock = (props) => {
+    return <h1>Display Stock data</h1>
+}
+
+export default Stock
